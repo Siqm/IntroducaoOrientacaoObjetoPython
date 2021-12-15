99c99bdf2b78515345c89a00b2aeab9ae04151be
SiqMello <91890617+Siqm@users.noreply.github.com>
2021-12-15 11:41:34 -0300

    Códigos

---
+++ conta.py
@@ -0,0 +1,47 @@
+class Conta:
+
+    def __init__(self, num, titular, saldo=0.0, limite=1000.0):
+        self.__num = num
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
+
+    def extrato(self):
+        print(f'Olá {self.__titular}, seu saldo é de: R$ {self.__saldo}')
+
+    def depositar(self, valor):
+        self.__saldo += valor
+        print(f'Seu saldo agora é de R${self.__saldo}')
+
+    def __pode_sacar(self, valor_a_sacar):
+        valor_disponivel = self.__saldo + self.__limite
+        return valor_a_sacar <= valor_disponivel
+
+    def sacar(self, valor):
+        if self.__pode_sacar(valor):
+            self.__saldo -= valor
+            print(f'Seu saldo agora é de R${self.__saldo}')
+
+    def transferir(self, valor, destino):
+        self.sacar(valor)
+        destino.depositar(valor)
+
+    @property
+    def saldo(self):
+        return self.__saldo
+
+    @property
+    def titular(self):
+        return self.__titular
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
+
+    @staticmethod
+    def codigo_banco():
+        return '001'
